f1c4bd000aa120eb85a06ebc88e0542cdb5ed32a
kinto-b <61998181+kinto-b@users.noreply.github.com>
2024-01-16 09:59:44 +0000

    Deploying to gh-pages from @ kinto-b/kinto-b.github.io@81462bd1a58d0ee5f259baf3025f0b7eb4d20f2c 🚀

---
+++ _app/immutable/nodes/3.iCCgIbR5.js
@@ -0,0 +1,15 @@
+import{s as i,n as s}from"../chunks/scheduler.kAlY8CUs.js";import{S as l,i as c,o as m,p,q as u,m as g,n as d,r as h,e as f,c as b,u as _,g as y,d as $}from"../chunks/index.VppoLM5v.js";import{C as v}from"../chunks/Card.bSucBN_Z.js";const k=!1,w=k,I=w,C=!0,T=Object.freeze(Object.defineProperty({__proto__:null,csr:I,prerender:C},Symbol.toStringTag,{value:"Module"}));function S(r){let e,n=`Hello world! I&#39;m Kinto Behr, a statistician and data scientist living in Melbourne, Australia.
+
+        I am employed by the Social Research Centre where I work on a fairly wide variety of things, from exploring different approaches to gender in survey weighting, through R package development and maintenance, to application development.
+
+        <br/> <br/>
+
+        In my free time, I enjoy contributing to the open source community, hiking, and reading.
+
+        Currently, I&#39;m making my way through Robert Brandom&#39;s big book on Hegel, &#39;A Spirit of Trust&#39;.
+
+        At some stage I might get around to publishing my thoughts on it and other topics here on this website.
+
+        <br/> <br/>
+
+        Until then, you might like to see what I&#39;ve been doing on <a href="https://github.com/kinto-b">github</a> or drop me a line at kinto.behr ☀ gmail dot com.`;return{c(){e=f("p"),e.innerHTML=n},l(t){e=b(t,"P",{"data-svelte-h":!0}),_(e)!=="svelte-tb6ycj"&&(e.innerHTML=n)},m(t,o){y(t,e,o)},p:s,d(t){t&&$(e)}}}function x(r){let e,n;return e=new v({props:{title:"kinto behr",",":!0,titleLink:"/",$$slots:{default:[S]},$$scope:{ctx:r}}}),{c(){m(e.$$.fragment)},l(t){p(e.$$.fragment,t)},m(t,o){u(e,t,o),n=!0},p(t,[o]){const a={};o&1&&(a.$$scope={dirty:o,ctx:t}),e.$set(a)},i(t){n||(g(e.$$.fragment,t),n=!0)},o(t){d(e.$$.fragment,t),n=!1},d(t){h(e,t)}}}class A extends l{constructor(e){super(),c(this,e,null,x,i,{})}}export{A as component,T as universal};
