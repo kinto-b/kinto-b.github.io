f84bb56df5180991857a5e3c738c26d2479ea6cc
Kinto <kinto.behr@gmail.com>
2025-02-13 19:14:43 +0100

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		mdsvex({
-		extensions: ['.md']
-		})
-	],
-
 	extensions: ['.svelte', '.md'],
 
 	kit: {
